95c83a9aa4a403285c704637b22a637227ddb88c
dragon262 <dragon262@cepegra.be>
2025-05-15 09:32:29 +0200

    separater

--- first.js
+++ first.js
@@ -1,10 +1,11 @@
-window.onload = function () {
+document.addEventListener('DOMContentLoaded', () => {
 let lastName = 'Smith\'s'
 let firstName = 'Pierre'
 let birthYear = 1969
 let insctription = true //false
 lastName = 'Charlier'
-let hobbies = new Array ('tennis','cinema')
+const hobbies = new Array ('tennis','cinema')
+hobbies.push('piscine')
 let conjoint = {
  firstName: "Martine",
  lastName: "VG",
@@ -15,17 +16,17 @@ let conjoint = {
    street: 'Rue machin 9',
    zip:6024,
    city: "Gosselies"
- }
-}
+   }
+   }
 let template = `
 <div>
  <p>Je m'appelle ${firstName} ${lastName}</p>
  <p>J'ai ${2025-birthYear} ans</p>
- <p>Mon hobbies: ${hobbies}</p>
+ <p>Mon hobbies: ${hobbies.join(' | ')}</p>
  <p>Mon conjoint s'appeller ${conjoint.firstName} ${conjoint.lastName} </p>
 </div>
 `
 let sortie = document.querySelector('#sortie')
 console.log(sortie)
 sortie.innerHTML = template
-}
+})
