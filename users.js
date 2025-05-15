de24e105fe26209244b3870e4c4e1faec63e63c0
dragon262 <dragon262@cepegra.be>
2025-05-15 11:36:08 +0200

    list des users et en gris active photo

---
+++ users.js
@@ -0,0 +1,12 @@
+const vignettes = document.querySelectorAll('.vignette')
+const $bigImg = document.querySelector('.big')
+//console.log(vignettes)
+vignettes.forEach (vignette => {
+  vignette.addEventListener('click', event => {
+    const cible = event.target
+    console.log(cible.src)
+    document.querySelector('.active').classList.remove('active')
+    cible.classList.add('active')
+    $bigImg.src = cible.src
+  })
+})
